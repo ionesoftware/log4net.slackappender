c2ab62871a6f062d7e8a640b00424542ba4cca83
Vladyslav Chyzhevskyi <vladyslav@chyzhevskyi.com>
2019-05-11 16:57:03 +0200

    publish nuget package on nuget.org

--- IONESoftware.log4net.SlackAppender/Properties/AssemblyInfo.cs
+++ IONESoftware.log4net.SlackAppender/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IONESoftware.log4net.SlackAppender")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An appender for log4net loggin library to post logging events to Slack.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("IONE Software Sp. z o.o. [Ltd]")]
 [assembly: AssemblyProduct("IONESoftware.log4net.SlackAppender")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
